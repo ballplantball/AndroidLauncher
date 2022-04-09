37723745da3355649c11352511f406f1949f4821
Tom Hsiao <44054500+ballplantball@users.noreply.github.com>
2022-04-09 21:13:24 +0800

    4/9_完成app圖片抓取

--- AndroidTest.cs
+++ AndroidTest.cs
@@ -6,13 +6,13 @@ using UnityEngine.UI;
 
 public class AndroidTest : MonoBehaviour
 {
-    public Text debug_text;
+    public GameObject ApplicationIcon;
+
     public void LaunchApp(String _packageName)
     {
         AndroidJavaClass unityPlayer;
         AndroidJavaObject currentActivity;
         AndroidJavaObject packageManager;
-        //AndroidJavaObject launchIntent;
 
         unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
         currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
@@ -22,31 +22,37 @@ public class AndroidTest : MonoBehaviour
         AndroidJavaObject packages = packageManager.Call<AndroidJavaObject>("getInstalledApplications", flag);        
         int count = packages.Call<int>("size");        
         AndroidJavaObject[] links = new AndroidJavaObject[count];
+        //AndroidJavaObject[] icons = new AndroidJavaObject[count]; java回傳drawable無法get跟轉用
         string[] names = new string[count];
         string[] packageName = new string[count];
-        //icons = new AndroidJavaObject[count];
-        //List<byte[]> byteimg = new List<byte[]>();
         int ii = 0;
+        
         for (int i = 0; ii < count;)
         {
-            //get the object
             AndroidJavaObject currentObject = packages.Call<AndroidJavaObject>("get", ii);
             try
             {
-                //try to add the variables to the next entry                
                 links[i] = packageManager.Call<AndroidJavaObject>("getLaunchIntentForPackage", currentObject.Get<AndroidJavaObject>("processName"));
+                //icons[i] = packageManager.Call<AndroidJavaObject>("getApplicationIcon", currentObject);
+
+                //在Android Studio新增Class將drawable轉成byte回傳
+                var plugin = new AndroidJavaClass("com.tomhsiao.mylibrary.PluginClass");
+                byte[] decodedBytes = plugin.CallStatic<byte[]>("getIcon", packageManager, currentObject);
+                Texture2D texture = new Texture2D(1, 1, TextureFormat.ARGB32, false);
+                texture.LoadImage(decodedBytes);
+                Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(.5f, .5f));
+
                 names[i] = packageManager.Call<string>("getApplicationLabel", currentObject);
                 packageName[i] = currentObject.Get<string>("processName");
                 //icons[i] = packageManager.Call<AndroidJavaObject>("getApplicationIcon", currentObject);
-                //Debug.Log("(" + ii + ") " + i + " " + names[i]);
-                Debug.Log("(" + ii + ") " + i + " " + packageName[i]);
-                //go to the next app and entry
+                //Debug.Log("(" + ii + ") " + i + " " + names[i]);                
+                GameObject appIcon =  Instantiate(ApplicationIcon, GameObject.FindGameObjectWithTag("List").transform);
+                appIcon.GetComponent<Image>().sprite = sprite;
                 i++;
                 ii++;
             }
             catch
             {
-                //if it fails, just go to the next app and try to add to that same entry.
                 Debug.Log("skipped " + ii);
                 ii++;
             }
